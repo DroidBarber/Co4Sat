64160e65296392d9c8119a18959d50b2f3ff9b5e
Mehdi Barim <60230244+DroidBarber@users.noreply.github.com>
2021-07-26 15:05:48 +0200

    First commit !

---
+++ app/src/main/java/com/example/co4sat/Alertes.java
@@ -0,0 +1,14 @@
+package com.example.co4sat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Alertes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alertes);
+    }
+}
